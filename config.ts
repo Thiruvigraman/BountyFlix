32b042a517f3c5ce8b3dc31dde9c6f5a3a438086
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-01-02 23:06:13 +0530

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,9 @@
+export const BOT_TOKEN = Deno.env.get("BOT_TOKEN")!;
+export const ADMIN_IDS = new Set(
+  Deno.env.get("ADMIN_IDS")!.split(",").map(Number)
+);
+export const REDIS_URL = Deno.env.get("REDIS_URL")!;
+export const REDIS_TOKEN = Deno.env.get("REDIS_TOKEN")!;
+export const LOG_CHANNEL_ID = Deno.env.get("LOG_CHANNEL_ID")!;
+export const ANNOUNCE_CHANNEL_ID = Deno.env.get("ANNOUNCE_CHANNEL_ID")!;
+export const INDEX_CHANNEL_ID = Deno.env.get("INDEX_CHANNEL_ID")!; // SeriesWalker channel
