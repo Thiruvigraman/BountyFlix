fd253af6d8d983d3ca1b0c6758666c4ca556240c
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-01-03 00:02:58 +0530

    Update logging.ts

--- logging.ts
+++ logging.ts
@@ -1,15 +1,26 @@
   //logging.ts
 
+
 import { BOT_TOKEN, LOG_CHANNEL_ID } from "./config.ts";
 
-export async function sendLog(text: string) {
-  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+const API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+
+export enum LogType {
+  BOT = "🤖 Bot Status",
+  ADMIN = "🛠️ Admin Action",
+  FILE = "📂 File Access",
+}
+
+export async function sendLog(type: LogType, message: string) {
+  const text = `<b>${type}</b>\n${message}`;
+
+  await fetch(`${API}/sendMessage`, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({
       chat_id: LOG_CHANNEL_ID,
       text,
-      parse_mode: "Markdown"
-    })
+      parse_mode: "HTML",
+    }),
   });
 }
