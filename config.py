9bfdd021730c7657a30615f54bdf0b3af37d3eb9
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-01-11 14:32:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,12 @@
 
 import os
 
-# Telegram
 BOT_TOKEN = os.getenv("TOKEN")
-
-# MongoDB
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "telegram_bot"
 
-# Admin IDs (add yours here)
 ADMINS = [
-    6930579635,  # example
+    6778132055,
 ]
 
 def is_admin(user_id: int) -> bool:
