ae6f9914918fbcef38c04ae926e9d8cb059655ce
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-01-11 14:32:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,18 +6,23 @@ from config import MONGO_URI, DB_NAME
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
-# SHARED collection with old bot
+# SHARED with old bot
 users_col = db["users"]
 
 # BountyFlix collections
-admins_col = db["bountyflix_admins"]
-logs_col = db["bountyflix_logs"]
+titles_col = db["bountyflix_titles"]
 
 def get_all_user_ids():
     ids = []
     for u in users_col.find({}):
         if "user_id" in u:
             ids.append(u["user_id"])
-        elif "user id" in u:  # typo-safe
+        elif "user id" in u:
             ids.append(u["user id"])
-    return list(set(ids))  # remove duplicates
+    return list(set(ids))
+
+def add_title(name: str):
+    titles_col.insert_one({"name": name})
+
+def get_titles():
+    return list(titles_col.find({}))
