8cc952e5a9ef7249649a62084a6f2bfbbd17f7a6
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-05 20:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN not set")
+    raise RuntimeError("BOT_TOKEN not set")
 
 if not MONGO_URI:
-    raise ValueError("MONGO_URI not set")
+    raise RuntimeError("MONGO_URI not set")
